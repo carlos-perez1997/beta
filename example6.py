7c9b633d36657f0e9bceea8218c759dc84a9ac98
carlos <rcpg.97@gmail.com>
2025-07-02 20:15:47 -0500

    challenge example6

--- example6.py
+++ example6.py
@@ -13,8 +13,8 @@ def ged_dices():
     print(f"Dice 2: {dice2}")
 
     if dice1 % 2 == 0 and dice2 % 2 == 0:
-        print("  YOU WIND  ")
+        print(" -- YOU WIND -- ")
     else:
-        print(" YOU LOSE ")
+        print(" -- YOU LOSE --")
 
 ged_dices()
