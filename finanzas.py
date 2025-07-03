5734d4f99b9b6bb3a84a508228a8fbc6d809d93a
carlos <rcpg.97@gmail.com>
2025-07-02 20:25:51 -0500

    Spring1-FinanSmart-2025

---
+++ finanzas.py
@@ -0,0 +1,48 @@
+import random  
+
+Titulo = "=== simulacin finanzas ==="
+Separacion = "-" * 40
+Client_Count = 5
+
+Clientes = []
+
+for i in range(Client_Count):
+    print(f"Entering data for Client {i+1}")
+    name = input("Full name: ")
+    id = input("ID number: ")
+    Age = int(input("Age: "))
+    costos = float(input("Monthly expenses: "))
+    Cell = input("Mobile phone: ")
+    Address = input("Address: ")
+    Status = True  
+
+    ingreso = random.randint(1000000, 8000000)  
+    saldo = ingreso - costos  
+
+    client_info = {
+        "Name": name,
+        "ID": id,
+        "Age": Age,
+        "Monthly expenses": costos,
+        "Mobile phone": Cell,
+        "Address": Address,
+        "Activo": Status,
+        "Ingreso": ingreso,
+        "Saldo": saldo
+    }
+    Clientes.append(client_info)
+
+print(f"{Titulo}")
+for idx, client in enumerate(Clientes, start=1):
+    print(f"Cliente {idx}")
+    print(Separacion)
+    print(f"Name: {client['Name']}")
+    print(f"ID: {client['ID']}")
+    print(f"Age: {client['Age']}")
+    print(f"Monthly Expenses: ${client['Expenses']:,.2f}")
+    print(f"Monthly Income:   ${client['Income']:,.2f}")
+    print(f"Estimated Balance: ${client['Balance']:,.2f}")
+    print(f"Phone: {client['Phone']}")
+    print(f"Address: {client['Address']}")
+    print(f"Active Client: {client['Status']}")
+    print(Separacion)
